68ffaeffd1071b785184fd3706c6f2d70a2654e1
Dumindu04 <166401628+Dumindu04@users.noreply.github.com>
2024-12-21 09:09:34 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID: 'YOUR SESSION ID',
+SESSION_ID: 'HANSAMAL-MD=iMMjkbaa#IHOw_AsYjLx6u2sfWSNd8aKzuq9MRtshmfo0pFETLrI',
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? 'YOUR GIT TOKEN (gph_without)' : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USER_NAME: process.env.GITHUB_USER_NAME === undefined ? 'YOUR GIT USER NAME' : process.env.GITHUB_USER_NAME,
 OWNER_NUMBER: process.env.OWNER_NUMBER === undefined ? 'YOUR NUMBER' : process.env.OWNER_NUMBER
